941481d0e525f1349df1d5d446fcd2ce56fc15ca
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-05-12 09:04:23 +0530

    RemoveDuplicatesfromSortedArray.c

---
+++ RemoveDuplicatesfromSortedArray.c
@@ -0,0 +1,15 @@
+int removeDuplicates(int* nums, int numsSize) {
+    for(int i=0; i<numsSize-1; i++)
+    {
+        if(nums[i] == nums[i+1])
+        {
+            for(int j = i+1; j<numsSize-1; j++)
+            {
+                nums[j] = nums[j+1];
+            }
+            numsSize--;
+            i--;
+        }
+    }
+    return numsSize;
+}
